a5addc5512146c0292b0d07710ced2853f5c03d5
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-03-20 10:19:10 +0700

    uploading all files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const {Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online`);
+}) // access events, listens when our bot is ready
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+    console.log(message.channel)
+})
+
+client.login(TOKEN);
+
+
